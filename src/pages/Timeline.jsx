db7d3e29625efe855ea27c5b3680dbc91d358b9f
Inesh Dey <ineshdey@hotmail.com>
2025-01-06 10:22:39 -0800

    Update Timeline.jsx

--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -5,14 +5,14 @@ const Timeline = () => {
             <section className={'pt-36 pb-14 px-14 bg-background-900 text-text-50 '}>
                 <h1 className={'text-6xl font-bold'}>What we've done</h1>
             </section>
-            <div className={'relative'}>
-                <div className={'absolute mx-auto h-full w-full z-10 max-md:hidden'}>
+            <div className={'relative bg-background-900'}>
+                <div className={'absolute mx-auto h-full w-full z-10 max-sm:hidden'}>
                     <div className={'bg-background-default w-4 h-full mx-auto rounded-t-2xl'}>
 
                     </div>
                 </div>
                 <div
-                    className={'w-full h-screen bg-background-900 text-text-50 relative px-14 flex-row flex max-sm:flex-col items-center'}>
+                    className={'w-full h-screen max-sm:h-full max-sm:py-8 bg-background-900 text-text-50 relative px-14 flex-row flex max-sm:flex-col items-center'}>
                     <div className={'max-sm:w-full w-2/5 flex flex-col'}>
                         <h1 className={'text-8xl'}>
                             2021
@@ -29,12 +29,12 @@ const Timeline = () => {
                     </div>
                 </div>
                 <div
-                    className={'w-full h-screen bg-background-900 text-text-50 relative px-14 flex-row-reverse max-md:flex-col flex items-center'}>
+                    className={'w-full h-screen max-sm:h-full max-sm:py-8 bg-background-900 text-text-50 relative px-14 flex-row-reverse max-sm:flex-col flex items-center'}>
                     <div className={'max-sm:w-full w-2/5 flex flex-col'}>
                         <h1 className={'text-8xl'}>
                             2022
                         </h1>
-                        <h2 className={'font-bold text-4xl pb-2'}>Planning</h2>
+                        <h2 className={'font-bold text-4xl pb-2'}>Analysis and Connections</h2>
                         <p>Held the first ten meetings with the district to discuss project feasibility. Conducted a
                             helioscope analysis of the school to assess solar viability. Expanded campaign efforts to
                             involve district-level support. Milestone: established credibility and build momentum
@@ -43,12 +43,12 @@ const Timeline = () => {
                     </div>
                 </div>
                 <div
-                    className={'w-full h-screen bg-background-900 text-text-50 relative px-14 flex-row max-md:flex-col flex items-center'}>
+                    className={'w-full h-screen max-sm:h-full max-sm:py-8 bg-background-900 text-text-50 relative px-14 flex-row max-sm:flex-col flex items-center'}>
                     <div className={'max-sm:w-full w-2/5 flex flex-col'}>
                         <h1 className={'text-8xl'}>
                             2023
                         </h1>
-                        <h2 className={'font-bold text-4xl pb-2'}>Planning</h2>
+                        <h2 className={'font-bold text-4xl pb-2'}>Expanding</h2>
                         <p>Increased the number of district meetings to refine project goals. Discovered that Tesla STEM
                             is
                             already highly energy-efficient; a solar installation would not be cost-effective. Shifted
@@ -59,18 +59,18 @@ const Timeline = () => {
                     </div>
                 </div>
                 <div
-                    className={'w-full h-screen bg-background-900 text-text-50 relative px-14 flex-row-reverse max-sm:flex-col flex items-center'}>
+                    className={'w-full h-screen max-sm:h-full max-sm:py-8 max-sm:pb-32 bg-background-900 text-text-50 relative px-14 flex-row-reverse max-sm:flex-col flex items-center'}>
                     <div className={'max-sm:w-full w-2/5 flex flex-col'}>
                         <h1 className={'text-8xl'}>
                             2024
                         </h1>
-                        <h2 className={'font-bold text-4xl pb-2'}>Planning</h2>
+                        <h2 className={'font-bold text-4xl pb-2'}>Keeping our legacy</h2>
                         <p>Focus on passing the project to a new group of students to ensure continuity. Milestone:
                             Transitioning leadership and securing a sustainable future for the STEM Solar initiative
                             beyond graduation. </p>
                     </div>
                 </div>
-                <div className={'absolute h-64 bg-gradient-to-t from-zinc-900 to-transparent w-full bottom-0 z-20'}>
+                <div className={'absolute h-64 sm:bg-gradient-to-t from-zinc-900 to-transparent w-full bottom-0 z-20'}>
 
                 </div>
             </div>
