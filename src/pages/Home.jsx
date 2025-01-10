b167be1afc1533a9d4b59965372f6d35807f9933
Inesh Dey <ineshdey@hotmail.com>
2025-01-10 10:29:43 -0800

    finished current page - waiting on photos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,22 +7,22 @@ const IntroAnim = () => {
     useEffect(() => {
         gsap.fromTo(
             textRef.current,
-            { opacity: 0, y: 20 },
-            { opacity: 1, y: 0, duration: 1, ease: "power2.out" }
+            {opacity: 0, y: 20},
+            {opacity: 1, y: 0, duration: 1, ease: "power2.out"}
         );
     }, []);
 
     return (
         <div ref={textRef}>
-        <h1 className={"text-6xl font-bold pb-2"}>
-            Welcome to <span
-            className="text-7xl bg-gradient-to-r from-accent-300 to-primary-500 bg-clip-text text-transparent">STEM Solar</span>
-        </h1>
-        <h2 className={"text-xl pb-5"}>Students with a mission to make the world a better place.</h2>
-        <h2 className={"text-xl"}>“Empowering schools to adopt sustainable energy solutions”</h2>
+            <h1 className={"text-6xl font-bold pb-2"}>
+                Welcome to <span
+                className="text-7xl bg-gradient-to-r from-accent-300 to-primary-500 bg-clip-text text-transparent">STEM Solar</span>
+            </h1>
+            <h2 className={"text-xl pb-5"}>Students with a mission to make the world a better place.</h2>
+            <h2 className={"text-xl"}>“Empowering schools to adopt sustainable energy solutions”</h2>
         </div>
     )
-    ;
+        ;
 };
 const Home = () => {
     return (<>
@@ -39,34 +39,37 @@ const Home = () => {
                 <div
                     className={'xl:w-[70rem] w-full min-h-36 flex flex-col md:mx-auto md:py-16 md:flex-row flex-nowrap [&>*]:w-full [&>*]:p-5 [&>*]:relative [&>*]:transition-all [&>*]:cursor-pointer'}>
                     <a href={"/timeline"}
-                        className={'group bg-primary-100 hover:bg-background-default hover:outline -outline-offset-4 outline-4 outline-black overflow-clip'}>
+                       className={'group bg-primary-100 hover:bg-background-default hover:outline -outline-offset-4 outline-4 outline-black overflow-clip'}>
                         <h1 className={'text-3xl group-hover:font-bold transition-all pb-5'}>What we did</h1>
                         <p className={'text-lg'}>We have established credibility and built momentum through discussions
                             with decision makers.</p>
                         <div className={"w-full min-h-14 h-full"}>
-                            <span className={"material-symbols-outlined text-5xl absolute bottom-5 md:group-hover:right-[10%] md:right-[80%] max-md:left-5  transition-all"}>
+                            <span
+                                className={"material-symbols-outlined text-5xl absolute bottom-5 md:group-hover:right-[10%] md:right-[80%] max-md:left-5  transition-all"}>
                             arrow_right_alt
                             </span>
                         </div>
                     </a>
                     <a href={"/current"}
-                        className={'group bg-primary-200 hover:bg-background-default hover:outline -outline-offset-4 outline-4  outline-1 outline-black overflow-clip'}>
+                       className={'group bg-primary-200 hover:bg-background-default hover:outline -outline-offset-4 outline-4  outline-1 outline-black overflow-clip'}>
                         <h1 className={'text-3xl group-hover:font-bold transition-all pb-5'}>What we're doing</h1>
                         <p className={'text-lg'}>Tesla STEM’s solar efficiency has been determined to be standard. We
                             are working with Alcott Elementary School.</p>
                         <div className={"w-full min-h-14 h-full"}>
-                            <span className={"material-symbols-outlined text-5xl absolute bottom-5 max-md:left-5 md:group-hover:right-[10%] md:right-[80%]  transition-all"}>
+                            <span
+                                className={"material-symbols-outlined text-5xl absolute bottom-5 max-md:left-5 md:group-hover:right-[10%] md:right-[80%]  transition-all"}>
                             arrow_right_alt
                             </span>
                         </div>
                     </a>
                     <a href={"/future"}
-                        className={'group bg-primary-300 hover:bg-background-default hover:outline -outline-offset-4 outline-4  outline-1 outline-black overflow-clip'}>
+                       className={'group bg-primary-300 hover:bg-background-default hover:outline -outline-offset-4 outline-4  outline-1 outline-black overflow-clip'}>
                         <h1 className={'text-3xl group-hover:font-bold transition-all pb-5'}>What we'll do</h1>
                         <p className={'text-lg'}>Expand renewable energy advocacy to more schools, collaborate with
                             local government.</p>
                         <div className={"w-full min-h-14 h-full"}>
-                            <span className={"material-symbols-outlined text-5xl absolute bottom-5 md:group-hover:right-[10%] md:right-[80%] max-md:left-5  transition-all"}>
+                            <span
+                                className={"material-symbols-outlined text-5xl absolute bottom-5 md:group-hover:right-[10%] md:right-[80%] max-md:left-5  transition-all"}>
                             arrow_right_alt
                             </span>
                         </div>
@@ -85,12 +88,12 @@ const Home = () => {
                              alt="Pranav Takrani"/>
                         <div className={'w-full p-8 my-auto'}>
                             <h1 className={'text-2xl pb-4 font-bold'}>Who we are</h1>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                                ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-                                sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                                est laborum</p>
+                            <p>We are the founders of STEM solar. We started this nonprofit in 2021 when were just
+                                Freshman in high school. With a shared passion for sustainability and education. We saw
+                                an opportunity to make a real impact by helping schools transition to renewable energy
+                                through the installation of solar panels. With the help of Dr.Wrenchy and other
+                                Stakeholder's at Tesla STEM high school in Remond Washington, we turned our vision into
+                                reality.</p>
                         </div>
                     </div>
                     <div className={'flex flex-col md:flex-row'}>
@@ -110,7 +113,9 @@ const Home = () => {
                              alt="Pranav Takrani"/>
                         <div className={'w-full p-8 my-auto'}>
                             <h1 className={'text-2xl pb-4 font-bold'}>Pranav Takrani</h1>
-                            <p>Hi, I’m Pranav Takrani, co-founder of Tesla STEM solar. I’ve been mainly in charge of the technical aspects of the project, including various cleaning systems, but also managerial roles in the overall growth of the project.</p>
+                            <p>Hi, I’m Pranav Takrani, co-founder of Tesla STEM solar. I’ve been mainly in charge of the
+                                technical aspects of the project, including various cleaning systems, but also
+                                managerial roles in the overall growth of the project.</p>
                         </div>
                     </div>
                 </div>
--- src/pages/Progress.jsx
+++ src/pages/Progress.jsx
@@ -6,33 +6,69 @@ const Progress = () => {
             <section className={'pt-36 pb-14 px-14 bg-background-900 text-text-50'}>
                 <h1 className={'text-6xl font-bold'}>STEM Solar - Now</h1>
             </section>
-            <div className={'relative bg-background-900 text-text-50 px-[15%] sm:py-32 max-sm:py-4'}>
-                <div className={'flex flex-col-reverse md:flex-row items-center'}>
-                    <div className={'w-full md:w-1/2 p-4'}>
-                        <h2 className={'text-4xl font-bold pb-2'}>Website Development</h2>
-                        <p>
-                            Using React, Tailwind CSS, and Vercel, we have created a website that showcases our mission
-                            and progress, providing a platform for us to connect with potential partners and donors.
-                            Our website has a focus of maintaining visual appeal, while being accessible to all and
-                            following proper web standards to ensure a consistent design across all devices Along with
-                            this, we created a cohesive color scheme and logo to represent our organization.
-                        </p>
-                        <a href="/branding.pdf" className={"text-accent-300"}>→ Check out our branding</a>
+            <div className={'relative bg-background-900 text-text-50 px-[15%] sm:py-32 max-sm:py-4 max-sm:pb-16'}>
+                <div className={'max-w-screen-lg mx-auto'}>
+                    <div className={'flex flex-col-reverse md:flex-row items-center'}>
+                        <div className={'w-full md:w-1/2 p-4'}>
+                            <h2 className={'text-4xl font-bold pb-2'}>Website Development</h2>
+                            <p>
+                                Using React, Tailwind CSS, and Vercel, we have created a website that showcases our
+                                mission
+                                and progress, providing a platform for us to connect with potential partners and donors.
+                                Our website has a focus of maintaining visual appeal, while being accessible to all and
+                                following proper web standards to ensure a consistent design across all devices. Along
+                                with
+                                this, we created a cohesive color scheme and logo to represent our organization.
+                            </p>
+                            <a href="/branding.pdf" className={"text-accent-300"}>→ Check out our branding</a>
+                        </div>
+                        <div className={'w-full md:w-1/2 p-4'}>
+                            <img src="/website.png" alt="Future Plans"
+                                 className={'w-full h-auto rounded-lg shadow-inner'}/>
+                        </div>
                     </div>
-                    <div className={'w-full md:w-1/2 p-4'}>
-                        <img src="/website.png" alt="Future Plans" className={'w-full h-auto rounded-lg shadow-inner'}/>
+                    <div className={'flex flex-col-reverse md:flex-row-reverse items-center'}>
+                        <div className={'w-full md:w-1/2 p-4'}>
+                            <h2 className={'text-4xl font-bold pb-2'}>Solar Chargers</h2>
+                            <p>The Solar Chargers Team goal was to help reduce fossil fuel emissions and promote clean
+                                energy and fight the misconception that solar energy is not viable in the rainy Seattle
+                                area. They chose to do this by creating a smaller solar charger at Tesla Stem High
+                                School
+                                that would charge multiple students’ phones at once. Additionally, they made a shelf
+                                that
+                                allowed all students to use this solar charger easily during lunch or between classes.
+                                <br/>
+                                They managed to create a working 50-watt solar panel system that could charge up to 5
+                                phones
+                                at once. The system stabilized the 12-volt input from the solar panel at 5 volts to
+                                safely
+                                charge phones. Additionally, their system could store power in a battery to also charge
+                                phones while there was no sunlight. They aim to improve their system by making it larger
+                                and
+                                more accessible, hoping to promote solar energy as an option for households and schools
+                                within the district.
+                            </p>
+                        </div>
+                        <div className={'w-full md:w-1/2 p-4'}>
+                            <img src="/solarcharger.jpg" alt="Solar Charger"
+                                 className={'w-full h-auto rounded-lg aspect-square'}/>
+                        </div>
                     </div>
                 </div>
-                <div className={'flex flex-col-reverse md:flex-row-reverse items-center'}>
+                <div className={'flex flex-col-reverse md:flex-row items-center'}>
                     <div className={'w-full md:w-1/2 p-4'}>
                         <h2 className={'text-4xl font-bold pb-2'}>Education</h2>
                         <p>Through the Education project our goal was to essentially create immersive lesson plans
                             targeted at various ages and subclassifications of students ranging from elementary
-                            schoolers to high schoolers to teach them about the STEM Solar initiative as well as other
-                            environmental science issues. Through our project we created engaging activities to convey
-                            the educational content in a more interactive and fun manner to encourage more retention.
+                            schoolers to high schoolers to teach them about the STEM Solar initiative as well as
+                            other
+                            environmental science issues. Through our project we created engaging activities to
+                            convey
+                            the educational content in a more interactive and fun manner to encourage more
+                            retention.
                             Lastly, we collected responses from students to analyze the data regarding how much they
-                            comprehended, how they were impacted and personal changes they will make in their day-to-day
+                            comprehended, how they were impacted and personal changes they will make in their
+                            day-to-day
                             lives.
                         </p>
                     </div>
@@ -40,16 +76,53 @@ const Progress = () => {
                         <img src="/education.jpg" alt="Future Plans" className={'w-full h-auto rounded-lg'}/>
                     </div>
                 </div>
-                <div className={'flex flex-col-reverse md:flex-row items-center'}>
+                <div className={'flex flex-col-reverse md:flex-row-reverse items-center'}>
                     <div className={'w-full md:w-1/2 p-4'}>
-                        <h2 className={'text-4xl font-bold pb-2'}></h2>
-                        <p>Create a program that trains students in renewable energy, sustainable
-                            architecture, and environmental advocacy, ensuring future students are
-                            equipped to lead STEM Solar and similar initiatives
+                        <h2 className={'text-4xl font-bold pb-2'}>Cleaning Systems</h2>
+                        <p>
+                            Our team was asked to make a solution for the manual labor necessary to keep solar panels
+                            clean so as to maximize STEM Solar’s solar panel energy output while also limiting the
+                            expenses necessary to upkeep them. We made both a simple and complex design to solve this,
+                            the simple design being cheaper and easier to construct, and the complex design being more
+                            ideal in terms of function and effectiveness.
                         </p>
                     </div>
                     <div className={'w-full md:w-1/2 p-4'}>
-                        <img src="path/to/your/image.jpg" alt="Future Plans" className={'w-full h-auto rounded-lg'}/>
+                        <img src="/cleaning.jpg" alt="Future Plans" className={'w-full h-auto rounded-lg'}/>
+                    </div>
+                </div>
+                <div className={'flex flex-col-reverse md:flex-row items-center'}>
+                    <div className={'w-full md:w-1/2 p-4'}>
+                        <h2 className={'text-4xl font-bold pb-2'}>Solar @ Stem</h2>
+                        <p>
+                            The Solar @ STEM group’s goal was to survey the students and staff of Tesla STEM about their
+                            opinion and knowledge about solar energy. They decided to publish a Microsoft Forms survey
+                            for students and staff to complete and conducted in-person interviews with teachers to
+                            gather additional information about their knowledge and opinions of solar energy at home and
+                            in the school. They compiled all the data from these sources and data from other groups to
+                            create a STEM Solar section in the school’s Climate Action Plan.
+                        </p>
+                    </div>
+                    <div className={'w-full md:w-1/2 p-4'}>
+                        <img src="/cleaning.jpg" alt="Future Plans" className={'w-full h-auto rounded-lg'}/>
+                    </div>
+                </div>
+                <div className={'flex flex-col-reverse md:flex-row-reverse items-center'}>
+                    <div className={'w-full md:w-1/2 p-4'}>
+                        <h2 className={'text-4xl font-bold pb-2'}>Community Outreach</h2>
+                        <p>
+                            The goal of period 3 community outreach was to gain the support of administrative
+                            organizations for TS Solar projects to be implemented in schools, particularly Tesla STEM
+                            but also local comprehensive high schools. The team contacted many administrators, in
+                            particular those of Juanita High School, Lake Washington High School, and Kirkland City
+                            Council. They developed informative presentations about TS solar that could be used in the
+                            meetings. The plan was to present to JHS, LWHS, and Kirkland City Council to forge a
+                            partnership that the next group of students doing this unit can use to get approval or
+                            possibly funding for projects to be implemented.
+                        </p>
+                    </div>
+                    <div className={'w-full md:w-1/2 p-4'}>
+                        <img src="/cleaning.jpg" alt="Future Plans" className={'w-full h-auto rounded-lg'}/>
                     </div>
                 </div>
             </div>
